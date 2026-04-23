d955c833edf03bc9d9c153523e87ba983aa3aaed
zhongming Fan <zhongmingfan@canway.net>
2026-04-23 09:20:10 +0800

    feat: enhance Oracle database initialization with user management and configuration updates

---
+++ weops/pipe-tools/charts/k8s_dp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 部署监控对象
+object=oracle
+kubectl apply -f ./oracle-monitor-user-init-configmap.yaml -n $object
+kubectl apply -f ./oracleDB_11g/svc.yaml -n $object
+kubectl apply -f ./oracleDB_11g/statefulset.yaml -n $object
+kubectl apply -f ./oracleDB_12c/svc.yaml -n $object
+kubectl apply -f ./oracleDB_12c/statefulset.yaml -n $object
+kubectl apply -f ./oracleDB_18c/svc.yaml -n $object
+kubectl apply -f ./oracleDB_18c/pvc.yaml -n $object
+kubectl apply -f ./oracleDB_18c/statefulset.yaml -n $object
+kubectl apply -f ./oracleDB_19c/pvc.yaml -n $object
+kubectl apply -f ./oracleDB_19c/statefulset.yaml -n $object
+kubectl apply -f ./oracleDB_21c/pvc.yaml -n $object
+kubectl apply -f ./oracleDB_21c/statefulset.yaml -n $object
