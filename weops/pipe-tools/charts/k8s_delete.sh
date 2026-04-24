569a8ec3a18ab2e48b1b0af95e91214607139332
zhongming Fan <zhongmingfan@canway.net>
2026-04-24 09:31:54 +0800

    fix: uncomment Oracle database resource deletions in k8s_delete.sh

--- weops/pipe-tools/charts/k8s_delete.sh
+++ weops/pipe-tools/charts/k8s_delete.sh
@@ -2,15 +2,14 @@
 
 # 删除监控对象
 object=oracle
-#kubectl delete -f ./oracleDB_11g/statefulset.yaml -n $object --ignore-not-found
-#kubectl delete -f ./oracleDB_11g/svc.yaml -n $object --ignore-not-found
-#kubectl delete -f ./oracleDB_12c/statefulset.yaml -n $object --ignore-not-found
-#kubectl delete -f ./oracleDB_12c/svc.yaml -n $object --ignore-not-found
-#kubectl delete -f ./oracleDB_18c/statefulset.yaml -n $object --ignore-not-found
-#kubectl delete -f ./oracleDB_18c/pvc.yaml -n $object --ignore-not-found
-#kubectl delete -f ./oracleDB_18c/svc.yaml -n $object --ignore-not-found
+kubectl delete -f ./oracleDB_11g/statefulset.yaml -n $object --ignore-not-found
+kubectl delete -f ./oracleDB_11g/svc.yaml -n $object --ignore-not-found
+kubectl delete -f ./oracleDB_12c/statefulset.yaml -n $object --ignore-not-found
+kubectl delete -f ./oracleDB_12c/svc.yaml -n $object --ignore-not-found
+kubectl delete -f ./oracleDB_18c/statefulset.yaml -n $object --ignore-not-found
+kubectl delete -f ./oracleDB_18c/svc.yaml -n $object --ignore-not-found
 kubectl delete -f ./oracleDB_19c/statefulset.yaml -n $object --ignore-not-found
 kubectl delete -f ./oracleDB_19c/pvc.yaml -n $object --ignore-not-found
-#kubectl delete -f ./oracleDB_21c/statefulset.yaml -n $object --ignore-not-found
-#kubectl delete -f ./oracleDB_21c/pvc.yaml -n $object --ignore-not-found
+kubectl delete -f ./oracleDB_21c/statefulset.yaml -n $object --ignore-not-found
+kubectl delete -f ./oracleDB_21c/pvc.yaml -n $object --ignore-not-found
 kubectl delete configmap oracle-monitor-user-init -n $object --ignore-not-found
